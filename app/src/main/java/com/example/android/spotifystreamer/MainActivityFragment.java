6da0347a5a5b1c3746dab066647fd05e6d6001ff
Kai Chang <kai.chang@morningstar.com>
2015-07-06 20:46:01 -0500

    Add onEditorAction when clicking search after entering artist name, beginnings of SearchArtists AsyncTask

--- app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -1,16 +1,26 @@
 package com.example.android.spotifystreamer;
 
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
@@ -28,6 +38,24 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        EditText editText = (EditText) rootView.findViewById(R.id.input_search_artist);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+
+                // IME_ACTION_SEARCH on phone, IME_NULL on emulator keyboard
+                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_NULL) {
+                    //searchArtists(v.getText().toString());
+                    Log.v(LOG_TAG, v.getText().toString());
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
         String[] artistData = {
                 "Grizzly Bear",
                 "Dirty Projectors",
@@ -43,12 +71,27 @@ public class MainActivityFragment extends Fragment {
                 artists);
                 //new ArrayList<String>());
 
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
         // Get reference to ListView and bind the adapter to it
         ListView listView = (ListView) rootView.findViewById(R.id.listview_artists);
         listView.setAdapter(mArtistAdapter);
 
         return rootView;
+    }
+
+    private class SearchArtistsTask extends AsyncTask<String, Void, Void> {
+
+        private final String LOG_TAG = SearchArtistsTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(String... params) {
+            // Connect to Spotify API with the wrapper
+            SpotifyApi api = new SpotifyApi();
+
+            // Create a SpotifyService object
+            SpotifyService spotify = api.getService();
+            ArtistsPager results = spotify.searchArtists(params[0]);
+
+            return null;
+        }
     }
 }
