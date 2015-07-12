fce139c97f403b511e8df9981a3595a34997c0ab
Kai Chang <kai.chang@morningstar.com>
2015-07-11 19:36:13 -0500

    Use SearchView instead of EditText to handle artist search input

--- app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -5,15 +5,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -48,24 +45,22 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        SearchView searchView = (SearchView) rootView.findViewById(R.id.input_search_artist);
 
-        EditText editText = (EditText) rootView.findViewById(R.id.input_search_artist);
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
+            public boolean onQueryTextSubmit(String query) {
+                SearchArtistsTask searchArtistsTask = new SearchArtistsTask();
 
-                // IME_ACTION_SEARCH on phone, IME_NULL on emulator keyboard
-                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_NULL) {
-                    SearchArtistsTask searchArtistsTask = new SearchArtistsTask();
-                    String searchArtist = v.getText().toString();
+                mToast = Toast.makeText(getActivity(), query + " not found.", Toast.LENGTH_SHORT);
+                searchArtistsTask.execute(query);
+                return true;
+            }
 
-                    // Create toast in case artist is not found
-                    mToast = Toast.makeText(getActivity(), searchArtist + " not found.", Toast.LENGTH_SHORT);
-                    searchArtistsTask.execute(searchArtist);
-                    handled = true;
-                }
-                return handled;
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
 
