ad8bedfd3cd55e889c2678f51c0c4bbc869cc082
Kai Chang <kai.chang@morningstar.com>
2015-08-16 21:32:35 -0500

    Prepare and start media player to play preview track selected

--- app/src/main/java/com/example/android/spotifystreamer/PlayActivityFragment.java
+++ app/src/main/java/com/example/android/spotifystreamer/PlayActivityFragment.java
@@ -1,11 +1,15 @@
 package com.example.android.spotifystreamer;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -26,6 +30,24 @@ public class PlayActivityFragment extends Fragment {
         // Use preview track url to play track
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
             String previewUrl = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+            // Prepare and start media player
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+            try {
+                mediaPlayer.setDataSource(previewUrl);
+                mediaPlayer.prepareAsync(); // might take long! (for buffering, etc)
+            } catch (IOException e) {
+
+            }
+
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
         }
 
         return rootView;
